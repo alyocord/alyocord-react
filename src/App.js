245b2ef518f2d9e513ce7d7da9292f898c9d90e7
FunWithAlbiYT <albiyt@proton.me>
2022-11-05 16:31:27 +0100

    added files

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import { appUrl } from './Config';
+
+function App() {
+  return (
+    <center>
+      <br/> <img src={"//"+appUrl+'/cdn-1/favicon.ico'} height='75' width='75' alt="alyocord logo"/> <br/>
+      <h1>Alyocord</h1>
+      <button class='button' onClick={() => window.location.href = "//"+appUrl+"/app/"}>I'm sold! Give me the app already!</button> <br/>
+      <br/><br/>
+      <section>
+        <h1>Great community</h1>
+        <p>Our community is super kind and helpful!</p>
+      </section><br/>
+      <section>
+        <h1>Helpful staff</h1>
+        <p>Feel free to contact our staff if you have any issues.</p>
+      </section><br/>
+      <section >
+        <h1>Little to no lag</h1>
+        <p>Read your messages the second they are sent!</p>
+      </section><br/>
+      <section>
+        <h1>Clean UI</h1>
+        <p>Super clean and responsive UI.</p>
+      </section>
+   </center>
+  );
+}
+
+export default App;
