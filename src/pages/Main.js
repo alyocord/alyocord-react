a8256f4a6a0b05ca2a21bdc13103cafeb350b4f3
xavvvv <97399129+xavwashere@users.noreply.github.com>
2022-11-12 19:15:47 +0000

    Update Main.js

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -15,7 +15,7 @@ function Main() {
     <center>
       <br/> <img src={"//"+cdnUrl+'/cdn-1/favicon.ico'} height='75' width='75' alt="alyocord logo"/> <br/>
       <h1>Alyocord</h1>
-      <Button variant="contained" onClick={() => nav('/app')}>I'm sold! Give me the app already!</Button> <br/>
+      <Button variant="contained" onClick={() => nav('/app')}>Sign me up!</Button> <br/>
       <br/><br/>
       <Container>
         <h1>Great community</h1>
